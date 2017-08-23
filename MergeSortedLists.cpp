2a08d249dd9d4fbbc65696eaa0a5f7f59d77f5d5
ablebodied <ablebodied@users.noreply.github.com>
2017-08-23 16:31:53 +0800

    Create MergeSortedLists.cpp

---
+++ MergeSortedLists.cpp
@@ -0,0 +1,247 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode
+{
+    int       m_nValue;
+    ListNode* m_pNext;
+};
+
+ListNode* CreateListNode(int value)
+{
+    ListNode* pNode = new ListNode();
+    pNode->m_nValue = value;
+    pNode->m_pNext = nullptr;
+
+    return pNode;
+}
+
+void ConnectListNodes(ListNode* pCurrent, ListNode* pNext)
+{
+    if(pCurrent == nullptr)
+    {
+        printf("Error to connect two nodes.\n");
+        exit(1);
+    }
+
+    pCurrent->m_pNext = pNext;
+}
+
+void PrintListNode(ListNode* pNode)
+{ 
+    if(pNode == nullptr)
+    {
+        printf("The node is nullptr\n");
+    }
+    else
+    {
+        printf("The key in node is %d.\n", pNode->m_nValue);
+    }
+}
+
+void PrintList(ListNode* pHead)
+{
+    printf("PrintList starts.\n");
+    
+    ListNode* pNode = pHead;
+    while(pNode != nullptr)
+    {
+        printf("%d\t", pNode->m_nValue);
+        pNode = pNode->m_pNext;
+    }
+
+    printf("\nPrintList ends.\n");
+}
+
+void DestroyList(ListNode* pHead)
+{
+    ListNode* pNode = pHead;
+    while(pNode != nullptr)
+    {
+        pHead = pHead->m_pNext;
+        delete pNode;
+        pNode = pHead;
+    }
+}
+
+void AddToTail(ListNode** pHead, int value)
+{
+    ListNode* pNew = new ListNode();
+    pNew->m_nValue = value;
+    pNew->m_pNext = nullptr;
+
+    if(*pHead == nullptr)
+    {
+        *pHead = pNew;
+    }
+    else
+    {
+        ListNode* pNode = *pHead;
+        while(pNode->m_pNext != nullptr)
+            pNode = pNode->m_pNext;
+
+        pNode->m_pNext = pNew;
+    }
+}
+
+ListNode* Merge(ListNode* pHead1, ListNode* pHead2)
+{
+    if(pHead1 == nullptr)
+        return pHead2;
+    else if(pHead2 == nullptr)
+        return pHead1;
+
+    ListNode* pMergedHead = nullptr;
+    ListNode* p1=pHead1;
+	ListNode* p2=pHead2;
+	if(p1->m_nValue<p2->m_nValue){
+	    pMergedHead=p1;
+		p1=p1->m_pNext;
+	}
+	else{
+	    pMergedHead=p2;
+		p2=p2->m_pNext;
+	
+	}
+	ListNode* p=pMergedHead;
+	while(p1!=nullptr&&p2!=nullptr){
+	    if(p1->m_nValue<p2->m_nValue){
+	    p->m_pNext=p1;
+		p=p->m_pNext;
+		p1=p1->m_pNext;
+	    }
+	    else{
+	    p->m_pNext=p2;
+	    p=p->m_pNext;
+		p2=p2->m_pNext;
+	
+	    }
+	}
+	if(p1==nullptr){
+	    p->m_pNext=p2;
+	}
+	else{
+	    p->m_pNext=p1;
+	}
+    
+
+    return pMergedHead;
+}
+
+// ====================测试代码====================
+ListNode* Test(char* testName, ListNode* pHead1, ListNode* pHead2)
+{
+    if(testName != nullptr)
+        printf("%s begins:\n", testName);
+
+    printf("The first list is:\n");
+    PrintList(pHead1);
+
+    printf("The second list is:\n");
+    PrintList(pHead2);
+
+    printf("The merged list is:\n");
+    ListNode* pMergedHead = Merge(pHead1, pHead2);
+    PrintList(pMergedHead);
+    
+    printf("\n\n");
+
+    return pMergedHead;
+}
+
+// list1: 1->3->5
+// list2: 2->4->6
+void Test1()
+{
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode3 = CreateListNode(3);
+    ListNode* pNode5 = CreateListNode(5);
+
+    ConnectListNodes(pNode1, pNode3);
+    ConnectListNodes(pNode3, pNode5);
+
+    ListNode* pNode2 = CreateListNode(2);
+    ListNode* pNode4 = CreateListNode(4);
+    ListNode* pNode6 = CreateListNode(6);
+
+    ConnectListNodes(pNode2, pNode4);
+    ConnectListNodes(pNode4, pNode6);
+
+    ListNode* pMergedHead = Test("Test1", pNode1, pNode2);
+
+    DestroyList(pMergedHead);
+}
+
+// 两个链表中有重复的数字
+// list1: 1->3->5
+// list2: 1->3->5
+void Test2()
+{
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode3 = CreateListNode(3);
+    ListNode* pNode5 = CreateListNode(5);
+
+    ConnectListNodes(pNode1, pNode3);
+    ConnectListNodes(pNode3, pNode5);
+
+    ListNode* pNode2 = CreateListNode(1);
+    ListNode* pNode4 = CreateListNode(3);
+    ListNode* pNode6 = CreateListNode(5);
+
+    ConnectListNodes(pNode2, pNode4);
+    ConnectListNodes(pNode4, pNode6);
+
+    ListNode* pMergedHead = Test("Test2", pNode1, pNode2);
+
+    DestroyList(pMergedHead);
+}
+
+// 两个链表都只有一个数字
+// list1: 1
+// list2: 2
+void Test3()
+{
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(2);
+
+    ListNode* pMergedHead = Test("Test3", pNode1, pNode2);
+
+    DestroyList(pMergedHead);
+}
+
+// 一个链表为空链表
+// list1: 1->3->5
+// list2: 空链表
+void Test4()
+{
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode3 = CreateListNode(3);
+    ListNode* pNode5 = CreateListNode(5);
+
+    ConnectListNodes(pNode1, pNode3);
+    ConnectListNodes(pNode3, pNode5);
+
+    ListNode* pMergedHead = Test("Test4", pNode1, nullptr);
+
+    DestroyList(pMergedHead);
+}
+
+// 两个链表都为空链表
+// list1: 空链表
+// list2: 空链表
+void Test5()
+{
+    ListNode* pMergedHead = Test("Test5", nullptr, nullptr);
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+
+    return 0;
+}
