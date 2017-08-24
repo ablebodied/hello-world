a88cbf301d1866d3bb8afd0db34eb4aefd303fbf
ablebodied <ablebodied@users.noreply.github.com>
2017-08-24 15:54:12 +0800

    Create StackPushPopOrder.cpp

---
+++ StackPushPopOrder.cpp
@@ -0,0 +1,119 @@
+#include <cstdio>
+#include <stack>
+
+bool IsPopOrder(const int* pPush, const int* pPop, int nLength)
+{
+    if(pPush==nullptr||pPop==nullptr||nLength<=0)
+		return false;
+	bool bPossible = false;
+    std::stack<int> pushData;
+	const int* pPushNext=pPush;
+	const int* pPopNext=pPop;
+	
+	while(pPopNext-pPop<nLength){
+	    while(pushData.empty()||pushData.top()!=*pPopNext){
+		    if(pPushNext-pPush==nLength){
+			    break;
+			}
+				
+			
+			    pushData.push(*pPushNext);
+				pPushNext++;
+			
+		}
+		if(pushData.top()!=*pPopNext){
+		    break;
+		}
+		pushData.pop();
+		pPopNext++;
+	}
+	if(pPopNext-pPop==nLength)
+		bPossible=true;
+    
+    return bPossible;
+}
+
+// ====================测试代码====================
+void Test(const char* testName, const int* pPush, const int* pPop, int nLength, bool expected)
+{
+    if(testName != nullptr)
+        printf("%s begins: ", testName);
+
+    if(IsPopOrder(pPush, pPop, nLength) == expected)
+        printf("Passed.\n");
+    else
+        printf("failed.\n");
+}
+
+void Test1()
+{
+    const int nLength = 5;
+    int push[nLength] = {1, 2, 3, 4, 5};
+    int pop[nLength] = {4, 5, 3, 2, 1};
+    
+    Test("Test1", push, pop, nLength, true);
+}
+
+void Test2()
+{
+    const int nLength = 5;
+    int push[nLength] = {1, 2, 3, 4, 5};
+    int pop[nLength] = {3, 5, 4, 2, 1};
+    
+    Test("Test2", push, pop, nLength, true);
+}
+
+void Test3()
+{
+    const int nLength = 5;
+    int push[nLength] = {1, 2, 3, 4, 5};
+    int pop[nLength] = {4, 3, 5, 1, 2};
+    
+    Test("Test3", push, pop, nLength, false);
+}
+
+void Test4()
+{
+    const int nLength = 5;
+    int push[nLength] = {1, 2, 3, 4, 5};
+    int pop[nLength] = {3, 5, 4, 1, 2};
+    
+    Test("Test4", push, pop, nLength, false);
+}
+
+// push和pop序列只有一个数字
+void Test5()
+{
+    const int nLength = 1;
+    int push[nLength] = {1};
+    int pop[nLength] = {2};
+
+    Test("Test5", push, pop, nLength, false);
+}
+
+void Test6()
+{
+    const int nLength = 1;
+    int push[nLength] = {1};
+    int pop[nLength] = {1};
+
+    Test("Test6", push, pop, nLength, true);
+}
+
+void Test7()
+{
+    Test("Test7", nullptr, nullptr, 0, false);
+}
+ 
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+    Test6();
+    Test7();
+
+    return 0;
+}
