04e07bb3fcd2f6939b9990533ee0ec2b71866984
ablebodied <ablebodied@users.noreply.github.com>
2017-08-24 17:09:36 +0800

    Create PathInTree.cpp

---
+++ PathInTree.cpp
@@ -0,0 +1,366 @@
+#include <stack>
+#include <cstdio>
+#include <deque>
+#include <queue>
+#include <vector>
+
+struct BinaryTreeNode 
+{
+    int                    m_nValue; 
+    BinaryTreeNode*        m_pLeft;  
+    BinaryTreeNode*        m_pRight; 
+};
+BinaryTreeNode* CreateBinaryTreeNode(int value)
+{
+    BinaryTreeNode* pNode = new BinaryTreeNode();
+    pNode->m_nValue = value;
+    pNode->m_pLeft = nullptr;
+    pNode->m_pRight = nullptr;
+
+    return pNode;
+}
+
+void ConnectTreeNodes(BinaryTreeNode* pParent, BinaryTreeNode* pLeft, BinaryTreeNode* pRight)
+{
+    if(pParent != nullptr)
+    {
+        pParent->m_pLeft = pLeft;
+        pParent->m_pRight = pRight;
+    }
+}
+
+void PrintTreeNode(const BinaryTreeNode* pNode)
+{
+    if(pNode != nullptr)
+    {
+        printf("value of this node is: %d\n", pNode->m_nValue);
+
+        if(pNode->m_pLeft != nullptr)
+            printf("value of its left child is: %d.\n", pNode->m_pLeft->m_nValue);
+        else
+            printf("left child is nullptr.\n");
+
+        if(pNode->m_pRight != nullptr)
+            printf("value of its right child is: %d.\n", pNode->m_pRight->m_nValue);
+        else
+            printf("right child is nullptr.\n");
+    }
+    else
+    {
+        printf("this node is nullptr.\n");
+    }
+
+    printf("\n");
+}
+
+void PrintTree(const BinaryTreeNode* pRoot)
+{
+    PrintTreeNode(pRoot);
+
+    if(pRoot != nullptr)
+    {
+        if(pRoot->m_pLeft != nullptr)
+            PrintTree(pRoot->m_pLeft);
+
+        if(pRoot->m_pRight != nullptr)
+            PrintTree(pRoot->m_pRight);
+    }
+}
+
+void DestroyTree(BinaryTreeNode* pRoot)
+{
+    if(pRoot != nullptr)
+    {
+        BinaryTreeNode* pLeft = pRoot->m_pLeft;
+        BinaryTreeNode* pRight = pRoot->m_pRight;
+
+        delete pRoot;
+        pRoot = nullptr;
+
+        DestroyTree(pLeft);
+        DestroyTree(pRight);
+    }
+}
+void PrintFromTopToBottom(BinaryTreeNode* pRoot)
+{
+    if(pRoot == nullptr)
+        return;
+
+    std::deque<BinaryTreeNode *> dequeTreeNode;
+
+    dequeTreeNode.push_back(pRoot);
+
+    while(dequeTreeNode.size())
+    {
+        BinaryTreeNode *pNode = dequeTreeNode.front();
+        dequeTreeNode.pop_front();
+
+        printf("%d ", pNode->m_nValue);
+
+        if(pNode->m_pLeft)
+            dequeTreeNode.push_back(pNode->m_pLeft);
+
+        if(pNode->m_pRight)
+            dequeTreeNode.push_back(pNode->m_pRight);
+    }
+}
+
+void Print1(BinaryTreeNode* pRoot)
+{
+    if(pRoot == nullptr)
+        return;
+
+    std::queue<BinaryTreeNode*> nodes;
+    nodes.push(pRoot);
+    int nextLevel = 0;
+    int toBePrinted = 1;
+    while(!nodes.empty())
+    {
+        BinaryTreeNode* pNode = nodes.front();
+        printf("%d ", pNode->m_nValue);
+
+        if(pNode->m_pLeft != nullptr)
+        {
+            nodes.push(pNode->m_pLeft);
+            ++nextLevel;
+        }
+        if(pNode->m_pRight != nullptr)
+        {
+            nodes.push(pNode->m_pRight);
+            ++nextLevel;
+        }
+
+        nodes.pop();
+        --toBePrinted;
+        if(toBePrinted == 0)
+        {
+            printf("\n");
+            toBePrinted = nextLevel;
+            nextLevel = 0;
+        }
+    }
+}
+void Print(BinaryTreeNode* pRoot)
+{
+    if(pRoot == nullptr)
+        return;
+
+    std::stack<BinaryTreeNode*> firstStack;
+	std::stack<BinaryTreeNode*> secondStack;
+	firstStack.push(pRoot);
+	while(!(firstStack.empty()&&secondStack.empty())){
+	    while(!firstStack.empty()){
+		    BinaryTreeNode* pNode=firstStack.top();
+			firstStack.pop();
+			printf("%d\t",pNode->m_nValue);
+			if(pNode->m_pLeft!=nullptr)
+				secondStack.push(pNode->m_pLeft);
+			if(pNode->m_pRight!=nullptr)
+				secondStack.push(pNode->m_pRight);
+		}
+		printf("\n");
+		while(!secondStack.empty()){
+		    BinaryTreeNode* pNode=secondStack.top();
+			secondStack.pop();
+			printf("%d\t",pNode->m_nValue);
+			if(pNode->m_pRight!=nullptr)
+				firstStack.push(pNode->m_pRight);
+			if(pNode->m_pLeft!=nullptr)
+				firstStack.push(pNode->m_pLeft);
+			
+		}
+		printf("\n");
+	}
+}
+
+void FindPath(BinaryTreeNode* pRoot, int expectedSum, std::vector<int>& path);
+
+void FindPath(BinaryTreeNode* pRoot, int expectedSum)
+{
+    if(pRoot == nullptr)
+        return;
+
+    std::vector<int> path;
+    
+    FindPath(pRoot, expectedSum, path);
+}
+
+void FindPath
+(
+    BinaryTreeNode*   pRoot,        
+    int               expectedSum,  
+    std::vector<int>& path  )    
+   
+{
+    //currentSum += pRoot->m_nValue;
+    path.push_back(pRoot->m_nValue);
+
+    // 如果是叶结点，并且路径上结点的和等于输入的值
+    // 打印出这条路径
+    bool isLeaf = pRoot->m_pLeft == nullptr && pRoot->m_pRight == nullptr;
+    if( expectedSum==pRoot->m_nValue && isLeaf)
+    {
+        printf("A path is found: ");
+        std::vector<int>::iterator iter = path.begin();
+        for(; iter != path.end(); ++ iter)
+            printf("%d\t", *iter);
+        
+        printf("\n");
+    }
+
+    // 如果不是叶结点，则遍历它的子结点
+    if(pRoot->m_pLeft != nullptr)
+        FindPath(pRoot->m_pLeft, expectedSum-pRoot->m_nValue, path);
+    if(pRoot->m_pRight != nullptr)
+        FindPath(pRoot->m_pRight, expectedSum-pRoot->m_nValue, path);
+
+    // 在返回到父结点之前，在路径上删除当前结点，
+    // 并在currentSum中减去当前结点的值
+    //currentSum -= pRoot->m_nValue;
+    path.pop_back();
+} 
+
+// ====================测试代码====================
+void Test(char* testName, BinaryTreeNode* pRoot, int expectedSum)
+{
+    if(testName != nullptr)
+        printf("%s begins:\n", testName);
+
+    FindPath(pRoot, expectedSum);
+
+    printf("\n");
+}
+
+//            10
+//         /      \
+//        5        12
+//       /\        
+//      4  7     
+// 有两条路径上的结点和为22
+void Test1()
+{
+    BinaryTreeNode* pNode10 = CreateBinaryTreeNode(10);
+    BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+    BinaryTreeNode* pNode12 = CreateBinaryTreeNode(12);
+    BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+    BinaryTreeNode* pNode7 = CreateBinaryTreeNode(7);
+
+    ConnectTreeNodes(pNode10, pNode5, pNode12);
+    ConnectTreeNodes(pNode5, pNode4, pNode7);
+
+    printf("Two paths should be found in Test1.\n");
+    Test("Test1", pNode10, 22);
+
+    DestroyTree(pNode10);
+}
+
+//            10
+//         /      \
+//        5        12
+//       /\        
+//      4  7     
+// 没有路径上的结点和为15
+void Test2()
+{
+    BinaryTreeNode* pNode10 = CreateBinaryTreeNode(10);
+    BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+    BinaryTreeNode* pNode12 = CreateBinaryTreeNode(12);
+    BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+    BinaryTreeNode* pNode7 = CreateBinaryTreeNode(7);
+
+    ConnectTreeNodes(pNode10, pNode5, pNode12);
+    ConnectTreeNodes(pNode5, pNode4, pNode7);
+
+    printf("No paths should be found in Test2.\n");
+    Test("Test2", pNode10, 15);
+
+    DestroyTree(pNode10);
+}
+
+//               5
+//              /
+//             4
+//            /
+//           3
+//          /
+//         2
+//        /
+//       1
+// 有一条路径上面的结点和为15
+void Test3()
+{
+    BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+    BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+    BinaryTreeNode* pNode3 = CreateBinaryTreeNode(3);
+    BinaryTreeNode* pNode2 = CreateBinaryTreeNode(2);
+    BinaryTreeNode* pNode1 = CreateBinaryTreeNode(1);
+
+    ConnectTreeNodes(pNode5, pNode4, nullptr);
+    ConnectTreeNodes(pNode4, pNode3, nullptr);
+    ConnectTreeNodes(pNode3, pNode2, nullptr);
+    ConnectTreeNodes(pNode2, pNode1, nullptr);
+
+    printf("One path should be found in Test3.\n");
+    Test("Test3", pNode5, 15);
+
+    DestroyTree(pNode5);
+}
+
+// 1
+//  \
+//   2
+//    \
+//     3
+//      \
+//       4
+//        \
+//         5
+// 没有路径上面的结点和为16
+void Test4()
+{
+    BinaryTreeNode* pNode1 = CreateBinaryTreeNode(1);
+    BinaryTreeNode* pNode2 = CreateBinaryTreeNode(2);
+    BinaryTreeNode* pNode3 = CreateBinaryTreeNode(3);
+    BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+    BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+
+    ConnectTreeNodes(pNode1, nullptr, pNode2);
+    ConnectTreeNodes(pNode2, nullptr, pNode3);
+    ConnectTreeNodes(pNode3, nullptr, pNode4);
+    ConnectTreeNodes(pNode4, nullptr, pNode5);
+
+    printf("No paths should be found in Test4.\n");
+    Test("Test4", pNode1, 16);
+
+    DestroyTree(pNode1);
+}
+
+// 树中只有1个结点
+void Test5()
+{
+    BinaryTreeNode* pNode1 = CreateBinaryTreeNode(1);
+
+    printf("One path should be found in Test5.\n");
+    Test("Test5", pNode1, 1);
+
+    DestroyTree(pNode1);
+}
+
+// 树中没有结点
+void Test6()
+{
+    printf("No paths should be found in Test6.\n");
+    Test("Test6", nullptr, 0);
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+    Test6();
+
+    return 0;
+}
